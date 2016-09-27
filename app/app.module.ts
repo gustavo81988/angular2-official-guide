70697f9e111731f45a3b5a6a0835e39859a234e0
Gustavo Ramírez <gustavo81988@gmail.com>
2016-09-27 13:31:11 -0400

    User Input

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,37 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me.component';
+import {
+  KeyUpComponent_v1,
+  KeyUpComponent_v2,
+  KeyUpComponent_v3,
+  KeyUpComponent_v4
+} from './keyup.components';
+import { LittleTourComponent } from './little-tour.component';
+import { LoopbackComponent } from './loop-back.component';
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    ClickMeComponent,
+    KeyUpComponent_v1,
+    KeyUpComponent_v2,
+    KeyUpComponent_v3,
+    KeyUpComponent_v4,
+    LittleTourComponent,
+    LoopbackComponent
+  ],
+  providers: [
+
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { 
-    
-}
+export class AppModule { }
+
+
